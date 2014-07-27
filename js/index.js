94a2dfaa56c202fb93cb698170d6b269a693e001
pz3n <zyyap.2009@smu.edu.sg>
2014-07-27 21:31:35 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
       var amount = $("#amount").val() +" "+$("#currency").val();
       $("#amountResult").text(amount);
       $("#merchantResult").text($("#merchant").val());
-      $("#modeResult").text($("input[name=radioName]:checked").val());
+      $("#modeResult").text($("input[name=mode]:checked").val());
       
    });
    
