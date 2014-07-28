daf5b79c99c458863335f0abe5497545bacdf4ea
pz3n <zyyap.2009@smu.edu.sg>
2014-07-28 11:45:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,17 +7,17 @@ $( document ).ready(function() {
       
       var amount;
       if($("#amount").val()){
-         amount = "Amount: 0" +" "+$("#currency").val();
+         amount = $("#amount").val();
       }else{
-         amount = "Amount: "+ $("#amount").val() +" "+$("#currency").val();
+         amount = 0;
       }
       var merchant = "Merchant: "+$("#merchant").val()
       var mode = "Mode: "+$("input[name=mode]:checked").val()
-      $("#amountResult").text(amount);
+      $("#amountResult").text("Amount: 0" +" "+$("#currency").val());
       $("#merchantResult").text(merchant);
       $("#modeResult").text(mode);
       
-      var card1Result =  Math.random();
+      var card1Result =  Math.floor(Math.random()*10*amount);
       $("#card1").text(card1Result+" miles*");
       
    });
