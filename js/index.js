c1a5c4ed61cff9792f00e395f6362e30f2e709de
pz3n <zyyap.2009@smu.edu.sg>
2014-08-03 17:06:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -79,7 +79,7 @@ $( document ).ready(function() {
       
       for(var cardResult in cardResults){
          $("#js-card-result").append(
-            '<div class="highlight"><img src='+cardResult.link+' width="125" height="45"><span class="col-xs-6" id="card1" style="width:100px"></span><button class="btn btn-warning">Use It!</button></div>'
+            '<div class="highlight"><img src="http://www.airfares.com.sg/promotions/credit-card-deals/anz/img/r-travelcard.jpg" width="125" height="45"><span class="col-xs-6" id="card1" style="width:100px"></span><button class="btn btn-warning">Use It!</button></div>'
          );
       }
       
