a65d194166da6a6407a7469eb375c5ab081edbc3
pz3n <zyyap.2009@smu.edu.sg>
2014-08-03 17:04:26 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $( document ).ready(function() {
        {
            name: "PRVI Miles",
            coy: "UOB",
-           link:"http://www.google.com.sg/imgres?imgurl=http%3A%2F%2Fcdn.expatfinder.com%2Fdata%2Fimgs%2Fproduits%2Fresize150%2Fuob-prvi-miles-platinum-card_135723442203228400.jpg&imgrefurl=http%3A%2F%2Fcredit.expatfinder.com%2Fcredit-card-uob-prvi-miles-platinum-card_212_p615&h=116&w=180&tbnid=JxbNJtznQdZ2AM%3A&zoom=1&docid=aZzsmTCjAb-1GM&ei=rPPdU42VHIqjugSfx4CQCw&tbm=isch&ved=0CB8QMygFMAU&iact=rc&uact=3&dur=633&page=1&start=0&ndsp=46",
+           link:"http://t1.gstatic.com/images?q=tbn:ANd9GcTeyZ78NP7AQ2MjBEVAfOWqOsrFYiUNEJsE2m0KMihykjk7covD",
            rate: 2,
            minimum:5000,
            block: 5000
@@ -25,7 +25,7 @@ $( document ).ready(function() {
        {
            name: "Prestige MasterCard",
            coy: "Citibank",
-           link:"http://www.google.com.sg/imgres?imgurl=http%3A%2F%2F3.bp.blogspot.com%2F-WrHgQkOXJKg%2FUdet6apq_TI%2FAAAAAAAAEAo%2FOiRxQpBZbSM%2Fs1600%2FCitibank%252BPrestige%252BWorld%252BMasterCard%252BElite.jpg&imgrefurl=http%3A%2F%2Fgenerationsxyz.blogspot.com%2F2013%2F10%2Fnew-credit-cards-in-malaysia-2013.html&h=161&w=249&tbnid=qg1L-1g0vAYExM%3A&zoom=1&docid=aMNfUUKSX44rjM&ei=5_PdU-v7BNLJuASJzYCoAg&tbm=isch&ved=0CB0QMygDMAM&iact=rc&uact=3&dur=122&page=1&start=0&ndsp=48",
+           link:"http://3.bp.blogspot.com/-WrHgQkOXJKg/Udet6apq_TI/AAAAAAAAEAo/OiRxQpBZbSM/s1600/Citibank+Prestige+World+MasterCard+Elite.jpg",
            rate: 0.4,
            minimum:1250,
            block: 1250
@@ -34,23 +34,14 @@ $( document ).ready(function() {
        {
            name: "Travel Visa Signature",
            coy: "ANZ",
-           link: "http://www.google.com.sg/imgres?imgurl=http%3A%2F%2Fwww.airfares.com.sg%2Fpromotions%2Fcredit-card-deals%2Fanz%2Fimg%2Fr-travelcard.jpg&imgrefurl=http%3A%2F%2Fwww.airfares.com.sg%2Fpromotions%2Fcredit-card-deals%2Fanz%2Findex.htm&h=150&w=230&tbnid=5xkKSHUW01xKGM%3A&zoom=1&docid=R7M7waSl6jMz5M&ei=cPTdU-6YGoKiugSf1IFA&tbm=isch&ved=0CBsQMygBMAE&iact=rc&uact=3&dur=104&page=1&start=0&ndsp=36",
+           link: "http://www.airfares.com.sg/promotions/credit-card-deals/anz/img/r-travelcard.jpg",
            rate: 1,
            minimum:2000,
            block: 2000
        }
    ]
    
-   
-   
-   
-   $("#back").click(function(){
-      $(".js-index").show();
-      $(".js-result").hide();
-   });
-});
-
-$("#compare").click(function(){
+   $("#compare").click(function(){
       $(".js-index").hide();
       $(".js-result").show();
       
@@ -93,3 +84,14 @@ $("#compare").click(function(){
       }
       
    });
+
+   
+   
+   
+   
+   $("#back").click(function(){
+      $(".js-index").show();
+      $(".js-result").hide();
+   });
+});
+
