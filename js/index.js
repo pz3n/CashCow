064f8035e77cf850a0339ab0efdd6e2e5dd51d88
pz3n <zyyap.2009@smu.edu.sg>
2014-08-03 17:09:59 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -79,10 +79,10 @@ $( document ).ready(function() {
       
       for(var cardResult in cardResults){
          $("#js-card-result").append(
-            '<div class="highlight"><img src='
+            '<div class="highlight"><img src="'
          );
          $("#js-card-result").append(cardResult.link);
-         $("#js-card-result").append('width="125" height="45"><span class="col-xs-6" id="card1" style="width:100px"></span><button class="btn btn-warning">Use It!</button></div>');
+         $("#js-card-result").append('" width="125" height="45"><span class="col-xs-6" id="card1" style="width:100px"></span><button class="btn btn-warning">Use It!</button></div>');
       }
       
        
