7583e926f00f8516f3e681c26758615cbc108c28
pz3n <zyyap.2009@smu.edu.sg>
2014-08-06 22:26:41 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,74 +3,6 @@ $( document ).ready(function() {
   
    $("#dateInput").datepicker();
    
-   var merchants = [
-      {
-           name: "NTUC FairPrice",
-           category: "Groceries"
-       },
-   
-       {
-           name: "Golden Peony",
-           category: "Dining"
-       },
-   
-       {
-           name: "Man Fu Yuan",
-           category: "Dining"
-       }
-   ]
-   
-   var promos = [
-      {
-           card: "World MasterCard",
-           coy: "Maybank",
-           link:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR10QHSEk1HjEQdG1ZhKJNztSrPzz4iCBYqv-93Hxh-HaRd3dWw",
-           category: "Groceries",
-           rate: 40,
-           start: 20140806,
-           end: 20140825,
-           minimum:2500
-       }
-   ]
-   
-   var rates = [
-      {
-           card: "World MasterCard",
-           coy: "Maybank",
-           link:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR10QHSEk1HjEQdG1ZhKJNztSrPzz4iCBYqv-93Hxh-HaRd3dWw",
-           category: "Groceries",
-           rate: 0.4,
-           minimum:2500
-       },
-   
-       {
-           card: "PRVI Miles",
-           coy: "UOB",
-           link:"http://t1.gstatic.com/images?q=tbn:ANd9GcTeyZ78NP7AQ2MjBEVAfOWqOsrFYiUNEJsE2m0KMihykjk7covD",
-           category: "Groceries",
-           rate: 2,
-           minimum:5000
-       },
-   
-       {
-           card: "Prestige MasterCard",
-           coy: "Citibank",
-           link:"http://3.bp.blogspot.com/-WrHgQkOXJKg/Udet6apq_TI/AAAAAAAAEAo/OiRxQpBZbSM/s1600/Citibank+Prestige+World+MasterCard+Elite.jpg",
-           category: "Groceries",
-           rate: 0.4,
-           minimum:1250
-       },
-   
-       {
-           card: "Travel Visa Signature",
-           coy: "ANZ",
-           link: "http://www.airfares.com.sg/promotions/credit-card-deals/anz/img/r-travelcard.jpg",
-           category: "Dining",
-           rate: 1,
-           minimum:2000
-       }
-   ]
-   
    $("#compare").click(function(){
       $(".js-index").hide();
       $(".js-result").show();
@@ -152,14 +84,8 @@ $( document ).ready(function() {
          append += "</div>" ;
          $("#js-card-result").append(append);
       }
-      
-       
    });
 
-   
-   
-   
-   
    $("#back").click(function(){
       $(".js-index").show();
       $(".js-result").hide();
